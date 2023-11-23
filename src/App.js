6b9c240b9cdb091892c3de46451678a788b7e931
kushwaha-sonu <sonu9110063900@gmail.com>
2023-11-23 20:14:09 +0530

    day1 of learning react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex align-items-center justify-center">
+      <div className="card mt-5 border border-green-500 w-96 h-96">
+          <div className="card-header text-center bg-slate-600 py-5 text-2xl font-bold hover:bg-green-500 shadow-2xl text-white">
+            Login
+          </div>
+
+      </div>
     </div>
   );
 }
